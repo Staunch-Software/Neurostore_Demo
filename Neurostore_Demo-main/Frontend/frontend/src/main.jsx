620e26680691d0950d9c195118865579ee97a43c
renganath13 <renganath13@gmail.com>
2026-06-09 17:43:06 +0530

    App.jsx modified

--- Neurostore_Demo-main/Frontend/frontend/src/main.jsx
+++ Neurostore_Demo-main/Frontend/frontend/src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css' // Your global styles
 // 1. ADD THIS IMPORT
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* 2. WRAP YOUR APP IN BROWSERROUTER */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
 )
